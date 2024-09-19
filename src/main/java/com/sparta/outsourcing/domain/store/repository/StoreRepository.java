48a179d3fc928257fd25eac952cea4cce9ef731a
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-09-19 22:50:24 +0900

    refactor: 가게등록 delete여부도 확인하게 수정

--- src/main/java/com/sparta/outsourcing/domain/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/outsourcing/domain/store/repository/StoreRepository.java
@@ -1,10 +1,13 @@
 package com.sparta.outsourcing.domain.store.repository;
 
 import com.sparta.outsourcing.domain.store.entity.Store;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
     Optional<Store> findByName(String name);
+
+    List<Store> findAllByOwnerIdAndStatusFalse(Long ownerId);
 }
