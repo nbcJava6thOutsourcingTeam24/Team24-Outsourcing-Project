27aca0776a867937f339810c8c1ce462bd49f298
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-09-19 21:42:41 +0900

    update: 가게 생성 구현(user 주석처리)

--- src/main/java/com/sparta/outsourcing/domain/store/controller/StoreController.java
+++ src/main/java/com/sparta/outsourcing/domain/store/controller/StoreController.java
@@ -1,7 +1,12 @@
 package com.sparta.outsourcing.domain.store.controller;
 
+import com.sparta.outsourcing.domain.store.dto.request.StoreRequestDto;
 import com.sparta.outsourcing.domain.store.service.StoreService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class StoreController {
 
     private final StoreService storeService;
+
+    @PostMapping("/stores")
+    public ResponseEntity<String> createStore(@RequestBody StoreRequestDto storeRequestDto) {
+        storeService.createStore(storeRequestDto);
+        return new ResponseEntity<>("가게 생성이 완료되었습니다", HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/sparta/outsourcing/domain/store/service/StoreService.java
+++ src/main/java/com/sparta/outsourcing/domain/store/service/StoreService.java
@@ -1,12 +1,35 @@
 package com.sparta.outsourcing.domain.store.service;
 
+import com.sparta.outsourcing.domain.store.dto.request.StoreRequestDto;
+import com.sparta.outsourcing.domain.store.entity.Store;
 import com.sparta.outsourcing.domain.store.repository.StoreRepository;
+import com.sparta.outsourcing.domain.user.entity.User;
+import com.sparta.outsourcing.domain.user.enums.UserRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class StoreService {
 
     private final StoreRepository storeRepository;
+//    private final UserRepository userRepository;
+
+    public void createStore(StoreRequestDto storeRequestDto) {
+//        User user = User.fromAuthUser(authUser);
+
+//        User user = userRepository.findById(1L).orElseThrow(()->new IllegalArgumentException("User not found"));
+
+//        if(user.getUserRole() != UserRole.OWNER)
+//        {
+//            throw new IllegalArgumentException("Only owner can create store");
+//        }
+//
+//        storeRepository.findByName(storeRequestDto.getName())
+//            .orElseThrow(() -> new IllegalArgumentException("Store not found"));
+
+//        storeRepository.save(new Store(storeRequestDto, user));
+    }
 }
