455663c5ef8c6cfca84fbc4c0568748522a9721b
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-09-19 21:42:41 +0900

    update: 가게 생성 구현(user 주석처리)

--- src/main/java/com/sparta/outsourcing/domain/store/entity/Store.java
+++ src/main/java/com/sparta/outsourcing/domain/store/entity/Store.java
@@ -1,10 +1,15 @@
 package com.sparta.outsourcing.domain.store.entity;
 
 import com.sparta.outsourcing.domain.common.entity.Timestamped;
+import com.sparta.outsourcing.domain.store.dto.request.StoreRequestDto;
+import com.sparta.outsourcing.domain.user.entity.User;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,4 +27,16 @@ public class Store extends Timestamped {
     private LocalTime closeTime;
     private boolean status = false;
     private Integer minPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    public Store(StoreRequestDto storeRequestDto, User user) {
+        this.name = storeRequestDto.getName();
+        this.openTime = storeRequestDto.getOpenTime();
+        this.closeTime = storeRequestDto.getCloseTime();
+        this.minPrice = storeRequestDto.getMinPrice();
+        this.owner = user;
+    }
 }
