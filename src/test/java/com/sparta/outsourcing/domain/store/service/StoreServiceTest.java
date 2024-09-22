c8078abd82e4ba9251d13987f029bcc4708c0281
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-09-22 22:48:24 +0900

    test: 가게 수정 테스트코드 작성

--- src/test/java/com/sparta/outsourcing/domain/store/service/StoreServiceTest.java
+++ src/test/java/com/sparta/outsourcing/domain/store/service/StoreServiceTest.java
@@ -1,6 +1,7 @@
 package com.sparta.outsourcing.domain.store.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -41,9 +42,10 @@ class StoreServiceTest {
     @DisplayName("가게 생성 테스트 - 성공")
     void createStore_success() {
         // given
-        AuthUser authUser = new AuthUser(1L, "user@example.com", UserRole.OWNER);
-        User user = new User(authUser.getEmail(), "1234", authUser.getUserRole());
+        User user = new User("user@example.com", "1234", UserRole.OWNER);
         ReflectionTestUtils.setField(user, "id", 1L);
+
+        AuthUser authUser = new AuthUser(user.getId(), user.getEmail(), user.getUserRole());
 
         StoreRequestDto storeRequestDto = new StoreRequestDto(
             "가게이름",
@@ -66,11 +68,12 @@ class StoreServiceTest {
 
     @Test
     @DisplayName("가게 생성 테스트 - 실패 - 유저 권한 실패")
-    void createStore_fail_() {
+    void createStore_fail_userUnAuthorization() {
         //given
-        AuthUser authUser = new AuthUser(1L, "user@example.com", UserRole.USER);
-        User user = new User(authUser.getEmail(), "1234", authUser.getUserRole());
+        User user = new User("user@example.com", "1234", UserRole.USER);
         ReflectionTestUtils.setField(user, "id", 1L);
+
+        AuthUser authUser = new AuthUser(user.getId(), user.getEmail(), user.getUserRole());
 
         StoreRequestDto storeRequestDto = new StoreRequestDto(
             "가게이름",
@@ -94,9 +97,10 @@ class StoreServiceTest {
     @DisplayName("가게 생성 테스트 - 실패 - 가게가 3개 이상 있을때.")
     void createStore_fail_store_limit() {
         // given
-        AuthUser authUser = new AuthUser(1L, "user@example.com", UserRole.OWNER);
-        User user = new User(authUser.getEmail(), "1234", authUser.getUserRole());
+        User user = new User("user@example.com", "1234", UserRole.OWNER);
         ReflectionTestUtils.setField(user, "id", 1L);
+
+        AuthUser authUser = new AuthUser(user.getId(), user.getEmail(), user.getUserRole());
 
         StoreRequestDto storeRequestDto = new StoreRequestDto(
             "가게이름",
@@ -116,5 +120,120 @@ class StoreServiceTest {
         );
 
         assertEquals("가게는 최대 3개만 등록 가능합니다", exception.getMessage());
+        verify(storeRepository, times(1)).findAllByOwnerIdAndStatusFalse(user.getId());
+    }
+
+
+    @Test
+    @DisplayName("가게 수정 테스트 - 성공")
+    void updateStoere_sucess() {
+        // given
+        User user = new User("user@example.com", "1234", UserRole.OWNER);
+        ReflectionTestUtils.setField(user, "id", 1L);
+
+        AuthUser authUser = new AuthUser(user.getId(), user.getEmail(), user.getUserRole());
+
+        StoreRequestDto storeRequestDto = new StoreRequestDto(
+            "가게이름",
+            LocalTime.parse("12:00"),
+            LocalTime.parse("18:00"),
+            18000,
+            "공지입니다"
+        );
+
+        StoreRequestDto storeUpdateRequestDto = new StoreRequestDto(
+            "수정이름",
+            LocalTime.parse("13:00"),
+            LocalTime.parse("19:00"),
+            10000,
+            "수정된 공지입니다"
+        );
+
+        Store store = new Store(storeRequestDto, user);
+        ReflectionTestUtils.setField(store, "id", 1L);
+
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        when(storeRepository.findById(store.getId())).thenReturn(Optional.of(store));
+
+        store.update(storeUpdateRequestDto);
+
+        // when
+        storeService.updateStore(authUser, 1L, storeUpdateRequestDto);
+
+        // then
+        assertEquals("수정이름", store.getName());
+        assertEquals(LocalTime.parse("13:00"), store.getOpenTime());
+        assertEquals(LocalTime.parse("19:00"), store.getCloseTime());
+        assertEquals(10000, store.getMinPrice());
+        assertEquals("수정된 공지입니다", store.getNotice());
+    }
+
+    @Test
+    @DisplayName("가게 수정 테스트 - 실패 - 폐업상태인 가게 수정")
+    void updateStore_fail_closedStore() {
+        // given
+        User user = new User("user@example.com", "1234", UserRole.OWNER);
+        ReflectionTestUtils.setField(user, "id", 1L);
+
+        AuthUser authUser = new AuthUser(user.getId(), user.getEmail(), user.getUserRole());
+
+        StoreRequestDto storeRequestDto = new StoreRequestDto(
+            "가게이름",
+            LocalTime.parse("12:00"),
+            LocalTime.parse("18:00"),
+            18000,
+            "공지입니다"
+        );
+
+        Store store = new Store(storeRequestDto, user);
+        ReflectionTestUtils.setField(store, "id", 1L);
+        ReflectionTestUtils.setField(store, "status", true);
+
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        when(storeRepository.findById(store.getId())).thenReturn(Optional.of(store));
+
+        //when - then
+        ApplicationException exception = assertThrows(ApplicationException.class, () ->
+            storeService.updateStore(authUser, 1L, storeRequestDto)
+        );
+
+        assertEquals("가게가 존재하지 않습니다", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("가게 수정 테스트 - 실패 - user가 다를 경우.")
+    void updateStore_fail_user() {
+        // given
+        User user1 = new User("user@example.com", "1234", UserRole.OWNER);
+        ReflectionTestUtils.setField(user1, "id", 1L);
+
+        User user2 = new User("user@example.com", "1234", UserRole.OWNER);
+        ReflectionTestUtils.setField(user1, "id", 1L);
+
+        AuthUser authUser2 = new AuthUser(user2.getId(), user2.getEmail(), user2.getUserRole());
+
+        StoreRequestDto storeRequestDto = new StoreRequestDto(
+            "가게이름",
+            LocalTime.parse("12:00"),
+            LocalTime.parse("18:00"),
+            18000,
+            "공지입니다"
+        );
+
+        Store store = new Store(storeRequestDto, user1);
+        ReflectionTestUtils.setField(store, "id", 1L);
+
+        when(userRepository.findById(user2.getId())).thenReturn(Optional.of(user2));
+
+        when(storeRepository.findById(store.getId())).thenReturn(Optional.of(store));
+
+        //when - then
+        ApplicationException exception = assertThrows(ApplicationException.class, () ->
+            storeService.updateStore(authUser2, 1L, storeRequestDto)
+        );
+
+        assertEquals("계정의 권한이 없습니다.", exception.getMessage());
     }
 }
