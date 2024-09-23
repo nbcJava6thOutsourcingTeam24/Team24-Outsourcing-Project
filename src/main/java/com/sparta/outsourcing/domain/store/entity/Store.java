43100a8c1ccff8b93a6ea7de25fee05d3123306d
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-09-23 16:02:07 +0900

    refactor: merge를 위한 store 변경

--- src/main/java/com/sparta/outsourcing/domain/store/entity/Store.java
+++ src/main/java/com/sparta/outsourcing/domain/store/entity/Store.java
@@ -3,7 +3,6 @@ package com.sparta.outsourcing.domain.store.entity;
 import com.sparta.outsourcing.domain.common.entity.Timestamped;
 import com.sparta.outsourcing.domain.store.dto.request.StoreRequestDto;
 import com.sparta.outsourcing.domain.user.entity.User;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -23,17 +22,11 @@ public class Store extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
     private String name;
-    @Column(nullable = false)
     private LocalTime openTime;
-    @Column(nullable = false)
     private LocalTime closeTime;
     private boolean status = false;
-    @Column(nullable = false)
     private Integer minPrice;
-    private String notice;
-    private boolean isAdvertised = false;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "owner_id")
@@ -44,7 +37,6 @@ public class Store extends Timestamped {
         this.openTime = storeRequestDto.getOpenTime();
         this.closeTime = storeRequestDto.getCloseTime();
         this.minPrice = storeRequestDto.getMinPrice();
-        this.notice = storeRequestDto.getNotice();
         this.owner = user;
     }
 
@@ -53,14 +45,10 @@ public class Store extends Timestamped {
         this.openTime = storeRequestDto.getOpenTime();
         this.closeTime = storeRequestDto.getCloseTime();
         this.minPrice = storeRequestDto.getMinPrice();
-        this.notice = storeRequestDto.getNotice();
     }
 
     public void delete() {
         this.status = true;
     }
 
-    public void enableAdvertisement() {
-        this.isAdvertised = true;
-    }
 }
