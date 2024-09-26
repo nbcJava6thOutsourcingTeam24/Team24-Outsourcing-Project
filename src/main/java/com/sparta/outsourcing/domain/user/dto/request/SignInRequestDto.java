a0cd57055fad4a18d37f760c29cc7773828e23a2
kang <dmschd873@gmail.com>
2024-09-26 09:42:23 +0900

    Revert "[fix] user도메인 errorcode exception/errorcdoe 으로 수정"
    This reverts commit f3f3f548aeecceffc4ec96b0695b858dde917325.

--- src/main/java/com/sparta/outsourcing/domain/user/dto/request/SignInRequestDto.java
+++ src/main/java/com/sparta/outsourcing/domain/user/dto/request/SignInRequestDto.java
@@ -2,7 +2,6 @@ package com.sparta.outsourcing.domain.user.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,7 +10,7 @@ import lombok.Getter;
 public class SignInRequestDto {
 
     @NotBlank
-    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$", message = "이메일 형식은~@~.~를 지켜주세요.")
+    @Email
     public String email;
 
     @NotBlank
--- src/main/java/com/sparta/outsourcing/domain/user/dto/request/SignUpRequestDto.java
+++ src/main/java/com/sparta/outsourcing/domain/user/dto/request/SignUpRequestDto.java
@@ -4,7 +4,6 @@ package com.sparta.outsourcing.domain.user.dto.request;
 import com.sparta.outsourcing.domain.user.enums.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,7 +12,7 @@ import lombok.Getter;
 public class SignUpRequestDto {
 
     @NotBlank
-    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$", message = "이메일 형식은~@~.~를 지켜주세요.")
+    @Email
     private String email;
 
     @NotBlank
